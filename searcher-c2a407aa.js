19d9c209e2058162881a28fab655c008a3b647ba
Skxxtz <Skxxtz@users.noreply.github.com>
2026-03-18 10:21:16 +0000

    deploy: 80011a5ee65a77d9f8e91d31fb8bea970918a131

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-49462267.js',
+                path_to_root + 'searchindex-5ca3f0e6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
